5470aa83cda19031065cd8653daeb5a3d2f5b5be
jfoley <jmfoley@yahoo.com>
2013-05-31 19:57:55 -0400

    started on the ten coin tests

---
+++ db/TenCoin.js
@@ -0,0 +1,8 @@
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+var TYPES = require('tedious').TYPES;
+var dbConnect = require('./DbConnectionPool')
+var errMsg = '';
+
+
+function callback(error,results){};
